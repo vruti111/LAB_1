5e642c2e3ed50683add0a9292751179159da22ef
vruti01 <vsheladiya484@rku.ac.in>
2025-07-02 11:04:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace LAB_1
+{
+     internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+            Console.Read();
+        }
+    }
+
+}
---
+++ detail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class detail
+{
+ static void Main()
+	{
+		Console.WriteLine("____________________________");
+		Console.WriteLine("DETAILS");
+        Console.WriteLine("____________________________\n");
+		Console.WriteLine("NAME :Vruti Sheladiya");
+        Console.WriteLine("DOB : 1/11/2004");
+		Console.WriteLine("ADDRESS : 7 govind nagar kothariya main road");
+		Console.WriteLine("CITY : Rajkot");
+		Console.WriteLine("PINCODE : 36002");
+		Console.WriteLine("STSTE :Gujrat");
+		Console.WriteLine("COUNTRY : India");
+		Console.WriteLine("EMAIL : vruti@gmail.com")
+		Console.WriteLine("\n__________________________");
+		Console.ReadLine();
+
+
+    }
+}
