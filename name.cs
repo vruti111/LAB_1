322258c51a6d0ca6265d369286d9d0ae8e87fc02
vruti01 <vsheladiya484@rku.ac.in>
2025-07-07 22:23:36 +0530

    number likh 98971

--- name.cs
+++ name.cs
@@ -12,3 +12,87 @@ namespace LAB_1
 	}
 
 }
+/*using System;
+class Program
+{
+    static void Main()
+    {
+        int a = 0, b = 1, c;
+        Console.Write($"{a} {b} ");
+        for (int i = 2; i < 11; i++)
+        {
+            c = a + b;
+            Console.Write($"{c} ");
+            a = b;
+            b = c;
+        }
+    }
+}
+
+
+
+using System;
+class Program {
+    static void Main() {
+        Console.Write("Enter number: ");
+        string num = Console.ReadLine();
+        string[] words = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+        foreach (char c in num) {
+            int digit = c - '0';
+            Console.Write(words[digit] + " ");
+        }
+    }
+}
+
+using System;
+class Program {
+    static void Main() {
+        Console.Write("Enter number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int sum = 0, temp = num;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += digit * digit * digit;
+            temp /= 10;
+        }
+        if (sum == num)
+            Console.WriteLine("Armstrong Number");
+        else
+            Console.WriteLine("Not an Armstrong Number");
+    }
+}
+
+using System;
+class Program {
+    static void Main() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++)
+                Console.Write("*");
+            Console.WriteLine();
+        }
+    }
+}
+
+using System;
+class Program {
+    static void Main() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++)
+                Console.Write("*");
+            Console.WriteLine();
+        }
+    }
+}
+
+using System;
+class Program {
+    static void Main() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++)
+                Console.Write(j + " ");
+            Console.WriteLine();
+        }
+    }
+}*/
+
+
