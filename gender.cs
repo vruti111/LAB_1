c1c1c82c54d88550c68ddb7a21ab4d51173fd9cc
vruti01 <vsheladiya484@rku.ac.in>
2025-07-11 13:35:12 +0530

    gender..

--- gender.cs
+++ gender.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 namespace LAB_1
 {
-	public class gender
-	{
-		public static void Main(string[] args)
-		{
-			Console.Write("enter your name: ");
-			string name = Console.ReadLine();
-            Console.Write("enter your gender (M/F): ");
-			char gender = Convert.ToChar(Console.ReadLine().ToUpper());
-			string prefix = (gender == 'M') ? "Mr." : "Miss. ";
-			Console.WriteLine(" hy " + prefix + name);
+    public class gender
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("write your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter your gender (M/F)");
+            char gender = char.ToUpper(Convert.ToChar(Console.ReadLine()));
+            switch (gender)
+            {
+                case 'M':
+                    Console.WriteLine("mr. " + name);
+                    break;
+                case 'F':
+                    Console.WriteLine("ms. " + name);
+                    break;
+                default:
+                    Console.Write("gender is missing.");
+                    break;
+            }
         }
-	}
+    }
 }
