6b1ca173e0de2e1a51167ba1a4931cb68e7293bf
Vrutii <vrutip670@gmail.com>
2025-07-18 13:34:59 +0530

    lec 1 program

---
+++ lec1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+namespace LAB_1
+{
+	public class lec1
+	{
+		public static void Main(string[] args)
+
+		{
+			int[] a = { 1, 2, 5, 3, 4, 10, 20, 11, 12 };
+
+			Console.WriteLine("MAx number:"+a.Max());
+			Console.WriteLine("Min number:"+a.Min());
+
+			var reverse = a.Reverse();
+			Console.WriteLine("--------------------is");
+			foreach (int i in reverse)
+			{
+				Console.Write(i + " ");
+			}
+			Array.Sort(a);
+            Console.WriteLine("\nSorted is :");
+            foreach (int i in a)
+                Console.Write(i + " ");
+        }
+	}
+}
